3341e97da3f9e06bf1a518f67e4cc6733bf1a245
oztrkmrt <merttoztrk@gmail.com>
2024-10-04 01:21:13 +0300

    Mission Complete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,46 @@
 package org.example;
 
 
+import java.util.LinkedList;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println(checkForPalindrome("Was it a car or a cat I saw ?"));
+
+        System.out.println(convertDecimalToBinary(13));
+
+    }
+
+    public static boolean checkForPalindrome(String text){
+        String cleanedStr = text.replaceAll("[^a-zA-Z0-9]", "").toLowerCase(Locale.ENGLISH);
+        System.out.println(cleanedStr);
+        LinkedList<Character> charList = new LinkedList<>();
+        for (char c : cleanedStr.toCharArray()){
+            charList.add(c);
+        }
+        while (charList.size() > 1) {
+            if (!charList.pollFirst().equals(charList.pollLast())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static String convertDecimalToBinary(int decimal){
+        LinkedList<Integer> binaryList = new LinkedList<>();
+
+        while (decimal>0){
+            int reminder = decimal % 2;
+            binaryList.addFirst(reminder);
+            decimal = decimal / 2;
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int digit : binaryList){
+            stringBuilder.append(digit);
+        }
+        return stringBuilder.toString();
     }
 
 }
